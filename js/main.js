793876ad45271c1ab7d34a2cdcaa121e3e7d332b
Joseph Rodman <joseph.rodman@ilumenscloud.com>
2021-09-15 17:55:21 -0600

    Updates

--- js/main.js
+++ js/main.js
@@ -253,22 +253,5 @@
     })
   });
 
-  /** FORM STUFF **/
-  const form = document.getElementById("contact-form");
-
-const formEvent = form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  let mail = new FormData(form);
-  sendMail(mail);
-});
-
-const sendMail = (mail) => {
-  fetch("https://nodemailer-vic-lo.herokuapp.com/send", {
-    method: "post",
-    body: mail,
-  }).then((response) => {
-    return response.json();
-  });
-};
 
 })()
